7974a37de38a08ec18f63a607d554f61ca02524a
Pyrolistical <pyrogx1133@gmail.com>
2013-01-19 18:56:50 -0800

    xpath 1.13-7 from libxml-xpath-perl
    The xpath script included with Mac OS X is basically unusable.
    There is no -q flag to remove the prefix '-- NODE --' and attempts to pipe
    the stderr to /dev/null removes the new line separating the actual
    nodes found.
    
    This version of the script actually had a man page with useful
    information, unlike the one out of the box.

---
+++ xpath.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Xpath < Formula
+  homepage 'http://packages.debian.org/squeeze/libxml-xpath-perl'
+  url 'http://mirrors.us.kernel.org/ubuntu//pool/universe/libx/libxml-xpath-perl/libxml-xpath-perl_1.13-7_all.deb'
+  version '1.13-7'
+  sha1 '5d70f8054a7596727d05918b01b3617effe47937'
+
+  def install
+    system "tar xf data.tar.gz"
+    system "mkdir #{bin}"
+    system "cp usr/bin/xpath #{bin}"
+    system "mkdir -p #{man1}"
+    system "cp usr/share/man/man1//xpath.1p.gz #{man1}"
+  end
+end
